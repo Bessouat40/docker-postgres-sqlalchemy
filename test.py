28f4510a8a233e759c5d3a81d60d822ca0ef8a40
Roman Bessouat <labess40@MacBook-Air-de-Roman.local>
2023-01-15 21:07:04 +0100

    change query

--- test.py
+++ test.py
@@ -5,4 +5,7 @@ time.sleep(7)
 
 engine = create_engine("postgresql+psycopg2://postgres:postgres@db:5432/medicalDB")
 conn = engine.connect()
-conn.execute(text("INSERT INTO medicalTable (nom_patient, diagnostic) VALUES ('spongebob', 'pneumonia');"))
+query="INSERT INTO  medicalTable (nom_patient, diagnostic)  VALUES(%s,%s)"
+my_data=[('Michel', 'normal'),
+        ('Gerard', 'normal'),]
+id=conn.execute(query,my_data)
